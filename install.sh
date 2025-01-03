64aaee9603aecbe866a88ec76434b743a41b8c75
Yann Orlarey <orlarey@gmail.com>
2025-01-03 05:44:33 +0100

    Adds fcasmtool, a tool to generate the assembly code

--- install.sh
+++ install.sh
@@ -9,13 +9,14 @@ mkdir -p "$SHARE_DIR"
 
 # Copy files to installation directory
 
-cp fcbenchtool fcplottool fccomparetool fcanalyzetool fcdebugtool "$INSTALL_DIR"
+cp fcbenchtool fcplottool fccomparetool fcanalyzetool fcdebugtool fcasmtool "$INSTALL_DIR"
 
 chmod +x "$INSTALL_DIR/fcbenchtool"
 chmod +x "$INSTALL_DIR/fcplottool"
 chmod +x "$INSTALL_DIR/fccomparetool"
 chmod +x "$INSTALL_DIR/fcanalyzetool"
 chmod +x "$INSTALL_DIR/fcdebugtool"
+chmod +x "$INSTALL_DIR/fcasmtool"
 
 cp *_footer.cpp "$SHARE_DIR"
 cp *_header.cpp "$SHARE_DIR"
